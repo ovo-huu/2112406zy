a2aa3b87e4d7f369c8b88ed543551cb424694cdd
ovo-huu <119220147@qq.com>
2024-06-28 18:53:01 +0800

    1

--- AdminhomeView.vue
+++ AdminhomeView.vue
@@ -12,6 +12,7 @@
 
         
         
+
       </div>
       <router-link to="/adminhome/main-info" class="button">主要信息</router-link>
       <router-link to="/adminhome/under-sea" class="button">水下系统</router-link>
